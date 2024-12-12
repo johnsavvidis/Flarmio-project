2e4a7543b5cae3880dc6937044773f35477b6b1e
johnsavvidis <savvidisjohn1990@yahoo.gr>
2024-12-12 10:51:23 +0200

    updated project

---
+++ index.js
@@ -0,0 +1,29 @@
+window.onload = (event) => {
+    formSubmission();
+}
+function formSubmission(){  
+    let inputValue = document.getElementById("num_in").value;
+    let textMessage;
+    if(inputValue == ""){
+        document.getElementById("num_in").innerHTML = "";
+    }
+    else if(inputValue <= 10){
+        textMessage = "❌" + " Invalid input! Must be greater than 10!";
+        showPopupWindow(textMessage);
+        document.getElementById("num_in").value = null;
+    }
+    else{
+        textMessage = "✅" + " Input accepted!";
+        showPopupWindow(textMessage);
+        document.getElementById("num_in").value = null;
+    }
+}
+
+function showPopupWindow(message) {
+    document.getElementById("popupOverlay").style.display = "block";
+    document.getElementById("result").innerHTML = message;
+}
+
+function closePopupWindow() {
+    document.getElementById("popupOverlay").style.display = "none";
+}
